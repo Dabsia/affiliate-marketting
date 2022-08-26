db12e674eb43bb1793eaf2e84a7f2ee72c9de6b5
Dabsia <Dabojohnson98@gmail.com>
2022-08-25 23:49:12 -0700

    'Success'

---
+++ JS/main.js
@@ -0,0 +1,38 @@
+// // let btn = document.getElementById('btn')
+// // let main__btn = document.querySelector('.video__page')
+
+// // main__btn.addEventListener('click', takeToPoppop)
+
+// // function takeToPoppop(){
+// //     let main = document.querySelector('main')
+// //     let poppop = document.querySelector('.poppop')
+
+// //     main.style.display = 'none'
+// //     poppop.style.display = 'block'
+// // }
+
+
+// // btn.addEventListener('click', redirect)
+
+// // function redirect(e){
+// //     let fname = document.getElementById('fname');
+// //     let email = document.getElementById('email')
+// //     e.preventDefault()
+// //     if (fname.value.length > 0 && email.value.length > 0){
+        
+// //         window.location.href = 'https://app.expertnaire.com/product/6854060614/6665844856?p=1'
+
+// //     }
+    
+// //     else{
+// //         fname.style.border = '1px solid red';
+// //         email.style.border = '1px solid red'
+// //     }
+    
+// // }
+
+// let videoBtn = document.querySelector('.video__page')
+
+// videoBtn.addEventListener('click', () => {
+//     alert('Hello')
+// })
